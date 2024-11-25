e9f67ad14629cab09312a772c8d1fbbc7adcdf4c
Harry <harry@fumblebee.site>
2024-11-25 13:08:28 -0500

    added autocomplete

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # shell
 A little shell written in C
+
+# Building
+To build run this command
+'''
+gcc -o hsh src/shell.c -lreadline
+'''
+
+To run
+'''
+./hsh
+'''
--- src/shell.c
+++ src/shell.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 // builtins 
 int lsh_cd(char **args);
@@ -100,39 +102,11 @@ int lsh_execute(char **args)
 #define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
 {
-	int bufsize = LSH_RL_BUFSIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(char) * bufsize);
-	int c;
-
-	if (!buffer) {
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
+	char *line = readline("");
+	if (line && *line) {
+		add_history(line);
 	}
-
-	while (1) {
-		// Read a character
-		c = getchar();
-
-		// If we hit EOF, replace it with a null character and return.
-		if (c == EOF || c == '\n') {
-			buffer[position] = '\0';
-			return buffer;
-		} else {
-			buffer[position] = c;
-		}
-		position++;
-
-		// If we have exceeded the buffer, reallocate.
-		if (position >= bufsize) {
-			bufsize += LSH_RL_BUFSIZE;
-			buffer = realloc(buffer, bufsize);
-			if(!buffer) {
-				fprintf(stderr, "lsh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
+	return line;
 }
 
 #define LSH_TOK_BUFSIZE 64
@@ -183,6 +157,10 @@ void lsh_loop(void)
 			return;
 		}
 		line = lsh_read_line();
+		if (line == NULL) {
+			printf("\n");
+			break;
+		}
 		args = lsh_split_line(line);
 		status = lsh_execute(args);
 
